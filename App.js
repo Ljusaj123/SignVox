7c39b2e25079bd952a8d63bfff0b7aaa049b5376
Ljusaj123 <ljusajrosana@gmail.com>
2024-05-10 22:41:31 +0200

    initialized project, installed component library

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { name as appName } from "./app.json";
+import { Button } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Button
+        icon="camera"
+        mode="contained"
+        onPress={() => console.log("Pressed")}
+      >
+        Press me
+      </Button>
+    </PaperProvider>
   );
 }
+
+AppRegistry.registerComponent(appName, () => Main);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ readme.md
@@ -0,0 +1,13 @@
+# How to start?
+
+```sh
+npm install && npx run expo
+```
+
+# How to test?
+
+Install Expo Go on mobile phone an scan QR code or choose to run on web.
+
+### React Native paper - component library
+Documentation: [https://callstack.github.io/react-native-paper/](https://callstack.github.io/react-native-paper/)
+
